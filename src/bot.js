48e11cf9c3b5368e716913b3f5786a8236cecbf6
chivaughn <chivaughncharles1@gmail.com>
2020-12-15 16:39:12 -0400

    Player Updated

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const { Client, WebhookClient} = require('discord.js');
 const client = new Client();
 const PREFIX = "$";
+const { Player } = require("discord-player");
+const player = new Player(client);
+client.player = player;
 const webhookClient = new WebhookClient(
     process.env.WEBHOOK_ID,
     process.env.WEBHOOK_TOKEN
@@ -19,7 +22,7 @@ client.on('message', async message => {
         const [COMMAND, ...args] = message.content
         .trim()
         .substring(PREFIX.length)
-        .split(/\s+/);
+        .split(/ +/g);
 
         
         if (COMMAND == 'kick') {
@@ -63,6 +66,12 @@ client.on('message', async message => {
 
             let fortuneCookie = await getJoke();
             webhookClient.send(fortuneCookie);
+        } else if (COMMAND == 'play'){
+            let track = await client.player.play(message.member.voice.channel, args[0],message.member.user.tag);
+            message.channel.send(`Currently playing ${track.name}! - Requested by ${track.requestedBy}`);
+        } else if (COMMAND == 'stop') {
+            let track = await client.player.stop(message.guild.id);
+            message.channel.send(`STOPPED`);
         }
         
     };
